0a2b27a73b51cc0ea19aec1769c6685d72449cde
ravikant <ravidwivedi135581133@gmail.com>
2025-09-29 17:08:13 +0530

    Added changs

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,17 +1,10 @@
 module.exports = {
   apps: [
     {
-      name: "expense-tracker",
-      script: "src/server.ts",           // your entry point
-      interpreter: "./node_modules/.bin/ts-node",
-      watch: true,                       // restart on file changes
+      name: "server",
+      script: "dist/server.js", // compiled JS
+      watch: false,             // no need to watch in prod
       env: {
-        NODE_ENV: "development",
-        DATABASE_URL: "postgresql://admin:admin123@localhost:5432/tracker?schema=public",
-        REDIS_URL: "redis://localhost:6379",
-        PORT: 5000,
-      },
-      env_production: {
         NODE_ENV: "production",
         DATABASE_URL: "postgresql://admin:admin123@localhost:5432/tracker?schema=public",
         REDIS_URL: "redis://localhost:6379",
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import app from "./app";
 
 const PORT = process.env.PORT || 5000;
-
+app.get("/healthCheck",(req,res)=>{
+    res.send({message:"running"})
+})
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
