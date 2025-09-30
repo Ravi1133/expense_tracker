25d9b5abaed803b2ce3dfc9f02a67d5abb09520a
ravikant <ravidwivedi135581133@gmail.com>
2025-09-30 12:32:03 +0530

    added changes

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,7 @@ const PORT = process.env.PORT || 5000;
 app.get("/healthCheck",(req,res)=>{
     res.send({message:"running"})
 })
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+
+app.listen(5000, "0.0.0.0", () => {
+  console.log("Server running on port 5000");
 });
